89884d72dced2359f0affc9b00eb038b05956ce8
Sergey Slesarev <slqamsk@gmail.com>
2025-11-30 08:19:24 +0300

    New file src/test/java/demo/part01/SimpleWikiTest.java

---
+++ src/test/java/demo/part01/SimpleWikiTest.java
@@ -0,0 +1,18 @@
+package demo.part01;
+
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+public class SimpleWikiTest {
+    @Test
+    void test01() {
+        open("https://ru.wikipedia.org/wiki/Selenium");
+        $("body").shouldHave(text("WebDriver"));
+    }
+    @Test
+    void test02() {
+        open("https://ru.wikipedia.org/wiki/Selenium");
+        $("body").shouldHave(text("Selenide"));
+    }
+}
